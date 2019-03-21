4ae17dc65dfc92ebdc1722a12f24d869a6d4e5c1
Melissa Williams <melissa@brandmeetscopy.com>
2019-03-21 23:06:27 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,30 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  badges = []
+  array.each do |name|
+    badges << badge_maker(name)
+  end
+  return badges
+end
+
+
+def assign_rooms(array)
+  rooms = []
+  array.each_with_index do |name,index|
+    rooms << "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+  end
+  return rooms
+end
+
+
+def printer(attendees)
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
+  end
+  assign_rooms(attendees).each do |room|
+    puts room
+  end
+end
