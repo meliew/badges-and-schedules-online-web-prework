f265f155695556dbb638b39510374a15bb4f14ec
Melissa Williams <melissa@brandmeetscopy.com>
2019-03-21 20:35:27 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,7 +6,7 @@ names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 def batch_badge_creator(array)
   array.each do |name|
-    puts badge_maker
+    puts badge_maker(name)
 end
 end
 
