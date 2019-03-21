dba031c951868ccd4360b3eb4615affc2ce5f9c7
Melissa Williams <melissa@brandmeetscopy.com>
2019-03-21 20:22:30 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,4 +2,8 @@ def badge_maker(name)
   puts "Hello, my name is #{name}"
 end
 
-# Write your code here.
+names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def batch_badge_creator(array)
+  array.each {|n| puts "Hello, my name is #{name}"}
+end
